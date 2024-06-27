4aa921014c52411e9975cd15e384b3d53e9afe6e
pcorreia <pedro.correia@allcomsistemas.com>
2024-06-27 14:50:46 -0300

    feat: add promo banner to home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import CategoryList from "./_components/category-list";
 import Header from "./_components/header";
 import Search from "./_components/search-input";
@@ -11,6 +12,17 @@ const Home = () => {
       </div>
       <div className="px-5 pt-6">
         <CategoryList />
+      </div>
+      <div className="px-5 pt-6">
+        <Image
+          src="/promo-banner-01.png"
+          width={0}
+          height={0}
+          alt="Até 30% de desconto em pizzas"
+          className="w-full h-auto object-contain"
+          sizes="100vw"
+          quality={100}
+        />
       </div>
     </div>
   );
